c55e487cdede0ded9236326b16aaf75f51f371f5
MYLi <myli@scu.edu>
2024-07-17 09:40:59 -0700

    Can do multiple rows of metadata now

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import APIForm from "./components/APIForm";
 import Gallery from "./components/Gallery";
 import QueryResult from "./components/QueryResult";
 import ResolutionResult from "./components/ResolutionResult";
 import fs from "fs";
-import EditTemplate from "./utils/editTemplate";
+import EditTemplate from "./components/editTemplate";
 
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
@@ -18,86 +18,54 @@ const App = () => {
 	const [prevImages, setPrevImages] = useState([]);
 	const [response, setResponse] = useState({});
 	const [searchType, setSearchType] = useState("");
+	const [eidrs, setEIDRs] = useState([]);
 	const [editXML, setEditXML] = useState([]);
+	const [hasEditFormat, setHasEditFormat] = useState(false);
+	const [hasEpisodic, setHasEpisodic] = useState(false);
+	const [hasNonEpisodic, setHasNonEpisodic] = useState(false);
 
 	const callAPI = async (query, requestOptions) => {
 		const response = await fetch(query, requestOptions);
 		const text = await response.text(); // Changed from json() to text() to handle XML
 		const parser = new DOMParser();
 		const xmlDoc = await parser.parseFromString(text, "application/xml");
-		setEditXML(xmlDoc);
-		const rows = [];
-		const elements = xmlDoc.getElementsByTagName("BaseObjectData"); // Assuming main data is under BaseObjectData
-
-		for (let element of elements) {
-			const row = {
-				ID: element.getElementsByTagName("ID")[0].textContent,
-				StructuralType:
-					element.getElementsByTagName("StructuralType")[0].textContent,
-				Mode: element.getElementsByTagName("Mode")[0].textContent,
-				ReferentType:
-					element.getElementsByTagName("ReferentType")[0].textContent,
-				ResourceName:
-					element.getElementsByTagName("ResourceName")[0].textContent,
-				TitleClass: element
-					.getElementsByTagName("ResourceName")[0]
-					.getAttribute("titleClass"),
-				ReleaseDate: element.getElementsByTagName("ReleaseDate")[0].textContent,
-				Status: element.getElementsByTagName("Status")[0].textContent,
-				ApproximateLength:
-					element.getElementsByTagName("ApproximateLength")[0].textContent,
-				Registrant: element.getElementsByTagName("Registrant")[0].textContent,
-				Director: element
-					.getElementsByTagName("Director")[0]
-					.getElementsByTagName("md:DisplayName")[0].textContent,
-				Actor1: element
-					.getElementsByTagName("Actor")[0]
-					.getElementsByTagName("md:DisplayName")[0].textContent,
-				Actor2: element
-					.getElementsByTagName("Actor")[1]
-					.getElementsByTagName("md:DisplayName")[0].textContent,
-				Actor3: element
-					.getElementsByTagName("Actor")[2]
-					.getElementsByTagName("md:DisplayName")[0].textContent,
-				Actor4: element
-					.getElementsByTagName("Actor")[3]
-					.getElementsByTagName("md:DisplayName")[0].textContent,
-			};
-			rows.push(row);
+		//add the xmlDoc to setEditXML existing array
+		setEditXML((prev) => [...prev, xmlDoc]);
+		const baseObjectDatas = xmlDoc.getElementsByTagName("BaseObjectData");
+		if (baseObjectDatas.length > 0) {
+			const baseObjectData = baseObjectDatas[0];
+			let ReferentType;
+			if (
+				baseObjectData.getElementsByTagName("ReferentType") &&
+				baseObjectData.getElementsByTagName("ReferentType").length > 0
+			) {
+				ReferentType =
+					baseObjectData.getElementsByTagName("ReferentType")[0].textContent;
+				if (ReferentType === "Series" || ReferentType === "Season") {
+					setHasEpisodic(true);
+				}
+			}
 		}
-
-		// Convert rows to CSV
-		const csvHeaders = Object.keys(rows[0]).join(",") + "\n";
-		const csvRows = rows.map((row) => Object.values(row).join(",")).join("\n");
-		const csvContent = csvHeaders + csvRows;
-
-		// Create a Blob from the CSV content
-		const blob = new Blob([csvContent], { type: "text/csv" });
-		const url = URL.createObjectURL(blob);
-
-		// Create a link and trigger a download
-		const a = document.createElement("a");
-		a.href = url;
-		a.download = "data.csv"; // Specify the file name
-		document.body.appendChild(a); // Append the link to the document
-		a.click(); // Simulate a click on the link
-
-		// Clean up
-		document.body.removeChild(a);
-		URL.revokeObjectURL(url);
-		// Assuming the server response has a root element <response>
-		// const json = Array.from(xmlDoc.getElementsByTagName("response")).map(
-		// 	(node) => ({
-		// 		url: node.getElementsByTagName("url")[0]?.textContent, // Example of extracting data
-		// 		// Add other fields as needed
-		// 	})
-		// )[0]; // Simplified conversion to JSON, adjust according to actual XML structure
-
-		// if (json.url === null) {
-		// 	alert("Oops! Something went wrong with that query, let's try again!");
-		// } else {
-		// 	setResponse(json);
-		// }
+		const extraObjectMetadatas = xmlDoc.getElementsByTagName(
+			"ExtraObjectMetadata"
+		);
+		if (extraObjectMetadatas.length > 0) {
+			const extraObjectMetadata = extraObjectMetadatas[0];
+			if (extraObjectMetadata.getElementsByTagName("EditInfo")) {
+				setHasEditFormat(true);
+			} else {
+				if (
+					!extraObjectMetadata.getElementsByTagName("SeriesInfo") ||
+					!extraObjectMetadata.getElementsByTagName("SeasonInfo") ||
+					!extraObjectMetadata.getElementsByTagName("ClipInfo") ||
+					!extraObjectMetadata.getElementsByTagName("ManifestationInfo") ||
+					!extraObjectMetadata.getElementsByTagName("CompilationInfo") ||
+					!extraObjectMetadata.getElementsByTagName("EpisodeInfo")
+				) {
+					setHasNonEpisodic(true);
+				}
+			}
+		}
 	};
 
 	const makeQuery = () => {
@@ -114,6 +82,7 @@ const App = () => {
 				body: JSON.stringify({ title: { words: inputs.title } }),
 			};
 		} else {
+			setEIDRs((prev) => [...prev, inputs.eidr_id]);
 			query = `http://localhost:3001/api/resolve`;
 			requestOptions = {
 				...requestOptions,
@@ -171,15 +140,15 @@ const App = () => {
 				<div></div>
 			)}
 			<br></br>
-			<div className='container mx-auto px-4'>
-				{response && searchType === "byEidrId" && (
-					<ResolutionResult response={response} />
-				)}
-				{response && searchType === "byQuery" && (
-					<QueryResult items={response} />
-				)}
-			</div>
-			<EditTemplate xml={editXML} />
+
+			{eidrs.length > 0 && eidrs.map((eidr_id) => <div>{eidr_id}</div>)}
+			{hasEpisodic && (
+				<EditTemplate xmlArray={editXML} buttonName={"Episodic"} />
+			)}
+			{hasEditFormat && <EditTemplate xmlArray={editXML} buttonName={"Edit"} />}
+			{hasNonEpisodic && (
+				<EditTemplate xmlArray={editXML} buttonName={"NonEpisodic"} />
+			)}
 			<br></br>
 		</div>
 	);
---
+++ client/src/components/editTemplate.jsx
@@ -0,0 +1,233 @@
+import React from "react";
+import * as XLSX from "xlsx";
+import { saveAs } from "file-saver";
+import editTemplateFormat from "../assets/edittemplate.json";
+import episodicTemplate from "../assets/episodictemplate.json";
+import nonepisodicTemplate from "../assets/nonepisodictemplate.json";
+import excelToXMLMap from "./ExcelToXMLMap";
+
+const getDataRow = (xmlDoc, dataKeys) => {
+	const elements = xmlDoc.getElementsByTagName("BaseObjectData");
+	let row = [];
+	if (elements.length > 0) {
+		const baseObjectData = elements[0]; // Assuming we're only interested in the first BaseObjectData element
+
+		dataKeys.forEach((key) => {
+			const foundElements = baseObjectData.getElementsByTagName(
+				excelToXMLMap[key] || key
+			);
+			let value = "";
+			if (key === "Director 1") {
+				value =
+					baseObjectData.getElementsByTagName("Director").length > 0
+						? baseObjectData
+								.getElementsByTagName("Director")[0]
+								.getElementsByTagName("md:DisplayName")[0].textContent
+						: "";
+				row.push(value);
+			} else if (key === "Director 2") {
+				value =
+					baseObjectData.getElementsByTagName("Director").length > 1
+						? baseObjectData
+								.getElementsByTagName("Director")[1]
+								.getElementsByTagName("md:DisplayName")[0].textContent
+						: "";
+				row.push(value);
+			} else if (key === "Actor 1") {
+				value =
+					baseObjectData.getElementsByTagName("Actor").length > 0
+						? baseObjectData
+								.getElementsByTagName("Actor")[0]
+								.getElementsByTagName("md:DisplayName")[0].textContent
+						: "";
+				row.push(value);
+			} else if (key === "Actor 2") {
+				value =
+					baseObjectData.getElementsByTagName("Actor").length > 1
+						? baseObjectData
+								.getElementsByTagName("Actor")[1]
+								.getElementsByTagName("md:DisplayName")[0].textContent
+						: "";
+				row.push(value);
+			} else if (key === "Actor 3") {
+				value =
+					baseObjectData.getElementsByTagName("Actor").length > 2
+						? baseObjectData
+								.getElementsByTagName("Actor")[2]
+								.getElementsByTagName("md:DisplayName")[0].textContent
+						: "";
+				row.push(value);
+			} else if (key === "Actor 4") {
+				value =
+					baseObjectData.getElementsByTagName("Actor").length > 3
+						? baseObjectData
+								.getElementsByTagName("Actor")[3]
+								.getElementsByTagName("md:DisplayName")[0].textContent
+						: "";
+				row.push(value);
+			} else {
+				if (foundElements.length > 0) {
+					// Assuming we only care about the first found element for each key
+
+					// console.log(excelToXMLMap[key]);
+					value = foundElements[0].textContent || "";
+					row.push(value);
+				} else {
+					row.push(""); // Push an empty string if the element was not found
+				}
+			}
+		});
+	}
+
+	return row;
+};
+
+const EditTemplate = ({ xmlArray, buttonName }) => {
+	const generateExcelFromEditTemplate = (type) => {
+		// Prepare Excel data
+		const rows = [];
+		rows.push(["version:", "22"]); // Assuming version is static as per the requirement
+		if (type === "Edit") {
+			const metadataKeys = Object.keys(editTemplateFormat.metadata);
+			const metadataRequired = metadataKeys.map((key) => {
+				if (editTemplateFormat.metadata[key].required === "TBD") {
+					return "TBD";
+				}
+				return editTemplateFormat.metadata[key].required
+					? "required"
+					: "optional";
+			});
+			// rows.push(metadataKeys); // Third row: keys from metadata
+			rows.push(metadataRequired); // Second row: required or optional
+
+			// Data keys and their values
+			const dataKeys = Object.keys(editTemplateFormat.data);
+			const dataValues = dataKeys.map(
+				(key) => editTemplateFormat.data[key] || ""
+			); // Replace null with 'null' string
+			rows.push(dataKeys); // Third row: keys from data
+			xmlArray.forEach((xml) => {
+				rows.push(getDataRow(xml, dataKeys)); // Fourth row: values from data
+			});
+			// Generate Excel file
+			const wb = XLSX.utils.book_new();
+			const ws = XLSX.utils.aoa_to_sheet(rows);
+			XLSX.utils.book_append_sheet(wb, ws, "Edits"); // Set sheet name to "Edits"
+			const wbout = XLSX.write(wb, { bookType: "xlsx", type: "binary" });
+			function s2ab(s) {
+				const buf = new ArrayBuffer(s.length);
+				const view = new Uint8Array(buf);
+				for (let i = 0; i < s.length; i++) {
+					view[i] = s.charCodeAt(i) & 0xff;
+				}
+				return buf;
+			}
+
+			// Save to file
+			saveAs(
+				new Blob([s2ab(wbout)], { type: "application/octet-stream" }),
+				"editTemplate.xlsx"
+			);
+		} else if (type === "Episodic") {
+			const metadataKeys = Object.keys(episodicTemplate.metadata);
+			const metadataRequired = metadataKeys.map((key) => {
+				if (episodicTemplate.metadata[key].required === "TBD") {
+					return "TBD";
+				}
+				return episodicTemplate.metadata[key].required
+					? "required"
+					: "optional";
+			});
+			// rows.push(metadataKeys); // Third row: keys from metadata
+			rows.push(metadataRequired); // Second row: required or optional
+
+			// Data keys and their values
+			const dataKeys = Object.keys(episodicTemplate.data);
+			const dataValues = dataKeys.map(
+				(key) => episodicTemplate.data[key] || ""
+			); // Replace null with 'null' string
+			rows.push(dataKeys); // Third row: keys from data
+			xmlArray.forEach((xml) => {
+				rows.push(getDataRow(xml, dataKeys)); // Fourth row: values from data
+			});
+			// Generate Excel file
+			const wb = XLSX.utils.book_new();
+			const ws = XLSX.utils.aoa_to_sheet(rows);
+			XLSX.utils.book_append_sheet(wb, ws, "Episodic"); // Set sheet name to "Episodic"
+			const wbout = XLSX.write(wb, { bookType: "xlsx", type: "binary" });
+			function s2ab(s) {
+				const buf = new ArrayBuffer(s.length);
+				const view = new Uint8Array(buf);
+				for (let i = 0; i < s.length; i++) {
+					view[i] = s.charCodeAt(i) & 0xff;
+				}
+				return buf;
+			}
+
+			// Save to file
+			saveAs(
+				new Blob([s2ab(wbout)], { type: "application/octet-stream" }),
+				"Episodic.xlsx"
+			);
+		} else if (type === "NonEpisodic") {
+			const metadataKeys = Object.keys(nonepisodicTemplate.metadata);
+			const metadataRequired = metadataKeys.map((key) => {
+				if (nonepisodicTemplate.metadata[key].required === "TBD") {
+					return "TBD";
+				}
+				return nonepisodicTemplate.metadata[key].required
+					? "required"
+					: "optional";
+			});
+			// rows.push(metadataKeys); // Third row: keys from metadata
+			rows.push(metadataRequired); // Second row: required or optional
+
+			// Data keys and their values
+			const dataKeys = Object.keys(nonepisodicTemplate.data);
+			const dataValues = dataKeys.map(
+				(key) => nonepisodicTemplate.data[key] || ""
+			); // Replace null with 'null' string
+			rows.push(dataKeys); // Third row: keys from data
+			xmlArray.forEach((xml) => {
+				rows.push(getDataRow(xml, dataKeys)); // Fourth row: values from data
+			});
+			// Generate Excel file
+			const wb = XLSX.utils.book_new();
+			const ws = XLSX.utils.aoa_to_sheet(rows);
+			XLSX.utils.book_append_sheet(wb, ws, "NonEpisodic"); // Set sheet name to "Episodic"
+			const wbout = XLSX.write(wb, { bookType: "xlsx", type: "binary" });
+			function s2ab(s) {
+				const buf = new ArrayBuffer(s.length);
+				const view = new Uint8Array(buf);
+				for (let i = 0; i < s.length; i++) {
+					view[i] = s.charCodeAt(i) & 0xff;
+				}
+				return buf;
+			}
+
+			// Save to file
+			saveAs(
+				new Blob([s2ab(wbout)], { type: "application/octet-stream" }),
+				"NonEpisodic.xlsx"
+			);
+		}
+	};
+
+	// Example usage with a button click
+	const handleButtonClick = (type) => {
+		generateExcelFromEditTemplate(type);
+	};
+
+	return (
+		<div>
+			<button
+				className='text-white bg-black rounded-lg shadow-lg p-2 mt-4 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+				onClick={() => handleButtonClick(buttonName)}
+			>
+				Generate {buttonName} Excel
+			</button>
+		</div>
+	);
+};
+
+export default EditTemplate;
