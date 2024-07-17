9cba941ee20eec55095f9ae4ab00c64b2882e559
MYLi <myli@scu.edu>
2024-07-17 11:40:19 -0700

    Cors urls changed

--- server/server.js
+++ server/server.js
@@ -5,7 +5,14 @@ import xmlparser from "express-xml-bodyparser";
 
 const app = express();
 app.use(xmlparser());
-app.use(cors());
+app.use(
+	cors({
+		origin: [
+			"http://bmrtemplate-production-0181.up.railway.app",
+			"http://localhost:5173",
+		],
+	})
+);
 app.use(express.json());
 app.use("/api", recordRouter);
 app.get("/", (req, res) => {
