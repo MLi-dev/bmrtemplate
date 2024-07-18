427f627d1cda56b38d79628ca91a5efcc3cedd99
MYLi <myli@scu.edu>
2024-07-18 14:31:45 -0700

    Revert "Textarea for form, new column for unknown records, changed download button icon."
    This reverts commit 849e102500c465984b76ed5d46eedaf5fbd3da83.

--- client/src/components/APIForm.jsx
+++ client/src/components/APIForm.jsx
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
-//Original code
 const APIForm = ({ inputs, handleChange, onSubmit }) => {
 	const inputsInfo = [
-		"Input an ID to a piece of media (e.g. a movie, TV show, etc.)",
+		"Input a link to any website you would like to take a screenshot of. Do not include https or any protocol in the URL",
+		"Input eidr id here",
+		"Input true or false if you would like your website screenshot to not contain any ads",
+		"Input true or false if you would like your website screenshot to not contain of those annoying 'allow cookies' banners",
+		"Choose the width of your screenshot (in pixels)",
+		"Choose the height of your screenshot (in pixels)",
 	];
 	return (
 		<div className='max-w-screen-xl mx-auto px-8 text-center font-poppins'>
@@ -17,7 +21,7 @@ const APIForm = ({ inputs, handleChange, onSubmit }) => {
 						>
 							<div className='flex justify-between items-center mb-2'>
 								<label className='text-xl font-bold mr-2'>{category}</label>
-								<textarea
+								<input
 									type='text'
 									name={category}
 									value={value}
@@ -40,79 +44,5 @@ const APIForm = ({ inputs, handleChange, onSubmit }) => {
 		</div>
 	);
 };
+
 export default APIForm;
-
-// const APIForm = ({ onSubmit }) => {
-// 	// State to manage an array of IDs
-// 	const [inputs, setInputs] = useState([{ id: "" }]);
-
-// 	// Function to handle input change
-// 	const handleChange = (index, event) => {
-// 		const newInputs = [...inputs];
-// 		newInputs[index].id = event.target.value;
-// 		setInputs(newInputs);
-// 	};
-
-// 	// Function to add a new input field
-// 	const addInput = () => {
-// 		setInputs([...inputs, { id: "" }]);
-// 	};
-
-// 	// Function to remove an input field
-// 	const removeInput = (index) => {
-// 		const newInputs = [...inputs];
-// 		newInputs.splice(index, 1);
-// 		setInputs(newInputs);
-// 	};
-
-// 	// Modified onSubmit to handle an array of inputs
-// 	const handleSubmit = (event) => {
-// 		event.preventDefault();
-// 		onSubmit(inputs.map((input) => input.id));
-// 	};
-
-// 	return (
-// 		<div className='max-w-screen-xl mx-auto px-8 text-center font-poppins'>
-// 			<h2 className='text-2xl font-bold mb-4'>Enter the information:</h2>
-// 			<form
-// 				className='flex flex-col items-center relative p-4'
-// 				onSubmit={handleSubmit}
-// 			>
-// 				{inputs.map((input, index) => (
-// 					<div key={index} className='flex items-center mb-2'>
-// 						<textarea
-// 							type='text'
-// 							name={`id-${index}`}
-// 							value={input.id}
-// 							placeholder='Input an ID...'
-// 							onChange={(e) => handleChange(index, e)}
-// 							className='font-poppins border-2 border-gray-300 p-2 mr-2'
-// 						/>
-// 						<button
-// 							type='button'
-// 							onClick={() => removeInput(index)}
-// 							className='bg-red-500 text-white p-2'
-// 						>
-// 							Remove
-// 						</button>
-// 					</div>
-// 				))}
-// 				<button
-// 					type='button'
-// 					onClick={addInput}
-// 					className='bg-blue-500 text-white p-2 mb-4'
-// 				>
-// 					Add Another ID
-// 				</button>
-// 				<button
-// 					type='submit'
-// 					className='text-white bg-black rounded-lg shadow-lg p-2'
-// 				>
-// 					Search
-// 				</button>
-// 			</form>
-// 		</div>
-// 	);
-// };
-
-// export default { APIForm };
